edba6858e18104af9064075200352fa887b28fce
Hernandez-Alcan <hernandez05alcantara@gmail.com>
2024-09-19 20:46:26 -0600

    Add files via upload
    Signed-off-by: Hernandez-Alcan <hernandez05alcantara@gmail.com>

---
+++ Index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="es-es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Viajes.MX</title>
+</head>
+
+<body>
+    <div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div>
+
+    <div class="top-bar">
+        <div class="left-section">
+            <a href="https://enlace1.com">Enlace 1</a>
+            <a href="https://enlace2.com">Enlace 2</a>
+            <a href="https://enlace3.com">Enlace 3</a>
+            <a href="https://enlace4.com">Enlace 4</a>
+        </div>
+        <div class="right-section">
+            <img src="logotipo.png" alt="Logo" class="logo">
+        </div>
+    </div>
+
+    <div class="carousel">
+        <div class="carousel-track">
+            <div class="carousel-item">
+                <a href="https://item1.com">
+                    <img src="paris.jpeg" alt="Item 1">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="https://item2.com">
+                    <img src="puerto_vallarta.jpeg" alt="Item 2">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="https://item3.com">
+                    <img src="londres.jpeg" alt="Item 3">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="https://item4.com">
+                    <img src="moscu.jpeg" alt="Item 4">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="https://item5.com">
+                    <img src="athenas.jpeg" alt="Item 4">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="https://item5.com">
+                    <img src="tokio.jpeg" alt="Item 4">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="overlay">
+        <div class="grid-container">
+            <div class="item">
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-description">
+                        <p class="text-title"> Card Title</p>
+                        <p class="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Añade más elementos aquí -->
+        </div>
+    </div>
+</body>
+
+</html>
